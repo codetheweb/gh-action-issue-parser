681d450ffd90e0af72f2b65882bfb0c5565ea169
Max Isom <hi@maxisom.me>
2021-08-11 12:55:50 -0500

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import issueParser from 'issue-parser'
+
+const parse = issueParser('github')
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const issueBody: string = core.getInput('body')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const output = parse(issueBody)
 
-    core.setOutput('time', new Date().toTimeString())
+    core.info(`Parsed issue body: ${JSON.stringify(output)}`)
+
+    core.setOutput('parsed', output)
   } catch (error) {
     core.setFailed(error.message)
   }
